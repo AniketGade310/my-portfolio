56981188d533e2b74458de197925c06da4381ac2
Aniketgade <annycoder2020@gmail.com>
2026-06-06 11:07:09 +0530

    added deployment credentials

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: '2026-06-05',
   devtools: { enabled: true },
+  nitro:{
+    preset:'github-pages'
+  },
   app: {
     head: {
       title: 'Aniket Gade — Senior Software Product Analyst',
@@ -15,8 +18,10 @@ export default defineNuxtConfig({
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=DM+Mono:wght@300;400;500&family=Instrument+Serif:ital@0;1&display=swap' }
       ]
-    }
+    },
+    baseURL: '/my-portfolio/'
   },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  
 })
