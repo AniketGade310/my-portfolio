1ad39e9306b05d6793c6c63adb2366a2586d7b7a
Aniketgade <annycoder2020@gmail.com>
2026-06-06 16:00:39 +0530

    added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,10 +16,10 @@ export default defineNuxtConfig({
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=DM+Mono:wght@300;400;500&family=Instrument+Serif:ital@0;1&display=swap' }
-      ]
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=DM+Mono:wght@300;400;500&family=Instrument+Serif:ital@0;1&display=swap' },
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+      ],
     },
-    // baseURL: '/my-portfolio/'
   },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
